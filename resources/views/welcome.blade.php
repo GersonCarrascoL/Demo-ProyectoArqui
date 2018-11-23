05f3b9453fa73f9fe675a385bfe10f4fd77ae881
mateoromeoo <23mateoromero@gmail.com>
2018-11-23 15:29:01 -0500

    se agregó estilos a la tabla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,54 +12,59 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <div class="section_inicio1">
             <div class="container">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Inicio</th>
-                            <th>Destino</th>
-                            <th>Modelo</th>
-                            <th>Placa</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($response as $res)
-                            <tr>
-                                <th>{{ $res->driverName }}</th>
-                                <th>{{ $res->tripInitAddress }}</th>
-                                <th>{{ $res->tripEndAddress }}</th>
-                                <th>{{ $res->vehicleModel }}</th>
-                                <th>{{ $res->vehicleLicensePlate }}</th>
-                                <th>
-                                        {{-- action="{{ action('TripController@sendData') }}" --}}
-                                    <form  method="POST" class="form-data">
-                                        {{-- <input name="_token" type="hidden" value="{{ csrf_token() }}"/> --}}
-                                        <input type="hidden" name="idTrip" value="{{ $res->idTrip }}">
-                                        <input type="hidden" name="tripDate" value="{{ $res->tripDate }}">
-                                        <input type="hidden" name="tripInitLatitude" value="{{ $res->tripInitLatitude }}">
-                                        <input type="hidden" name="tripInitLongitude" value="{{ $res->tripInitLongitude }}">
-                                        <input type="hidden" name="tripEndLatitude" value="{{ $res->tripEndLatitude }}">
-                                        <input type="hidden" name="tripEndLongitude" value="{{ $res->tripEndLongitude }}">
-                                        <input type="hidden" name="tripInitAddress" value="{{ $res->tripInitAddress }}">
-                                        <input type="hidden" name="tripEndAddress" value="{{ $res->tripEndAddress }}">
-                                        <input type="hidden" name="idVehicle" value="{{ $res->idVehicle }}">
-                                        <input type="hidden" name="vehicleLicensePlate" value="{{ $res->vehicleLicensePlate }}">
-                                        <input type="hidden" name="vehicleModel" value="{{ $res->vehicleModel }}">
-                                        <input type="hidden" name="idDriver" value="{{ $res->idDriver }}">
-                                        <input type="hidden" name="driverName" value="{{ $res->driverName }}">
-                                        <input type="hidden" name="driverCellphone" value="{{ $res->driverCellphone }}">
-                                        <button type="submit" class="btn btn-primary">Send</button>
-                                    </form>
-                                </th>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="lista-cuadro col-xs-12 col-md-offset-1 col-md-10">
+                    <div class="lista-izq col-xs-12 col-sm-12">
+                        <table class="table table-striped listavuelos">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Inicio</th>
+                                    <th>Destino</th>
+                                    <th>Modelo</th>
+                                    <th>Placa</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($response as $res)
+                                    <tr>
+                                        <td>{{ $res->driverName }}</td>
+                                        <td>{{ $res->tripInitAddress }}</td>
+                                        <td>{{ $res->tripEndAddress }}</td>
+                                        <td>{{ $res->vehicleModel }}</td>
+                                        <td>{{ $res->vehicleLicensePlate }}</td>
+                                        <td>
+                                                {{-- action="{{ action('TripController@sendData') }}" --}}
+                                            <form  method="POST" class="form-data">
+                                                {{-- <input name="_token" type="hidden" value="{{ csrf_token() }}"/> --}}
+                                                <input type="hidden" name="idTrip" value="{{ $res->idTrip }}">
+                                                <input type="hidden" name="tripDate" value="{{ $res->tripDate }}">
+                                                <input type="hidden" name="tripInitLatitude" value="{{ $res->tripInitLatitude }}">
+                                                <input type="hidden" name="tripInitLongitude" value="{{ $res->tripInitLongitude }}">
+                                                <input type="hidden" name="tripEndLatitude" value="{{ $res->tripEndLatitude }}">
+                                                <input type="hidden" name="tripEndLongitude" value="{{ $res->tripEndLongitude }}">
+                                                <input type="hidden" name="tripInitAddress" value="{{ $res->tripInitAddress }}">
+                                                <input type="hidden" name="tripEndAddress" value="{{ $res->tripEndAddress }}">
+                                                <input type="hidden" name="idVehicle" value="{{ $res->idVehicle }}">
+                                                <input type="hidden" name="vehicleLicensePlate" value="{{ $res->vehicleLicensePlate }}">
+                                                <input type="hidden" name="vehicleModel" value="{{ $res->vehicleModel }}">
+                                                <input type="hidden" name="idDriver" value="{{ $res->idDriver }}">
+                                                <input type="hidden" name="driverName" value="{{ $res->driverName }}">
+                                                <input type="hidden" name="driverCellphone" value="{{ $res->driverCellphone }}">
+                                                <button type="submit" class="btn btn-primary">Send</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
                 <div class="message"></div>
             </div>
         </div>
